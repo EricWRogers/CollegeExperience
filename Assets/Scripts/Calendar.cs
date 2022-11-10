ffb328033daffaf9599b6f91c835d2febc7cf78b
Joshua Deaton <joshwdeaton@gmail.com>
2022-11-10 17:25:46 -0600

    Calendar Code

--- Assets/Scripts/Calendar.cs
+++ Assets/Scripts/Calendar.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Calendar 
 {
-    public enum DAY { DAY1, DAY2, DAY3, DAY4, DAY5, DAY6, DAY7 } //List of game states
-    // Start is called before the first frame update
+ 
+    public enum DAY { DAY1 = 1, DAY2 = 2, DAY3 = 3, DAY4 = 4, DAY5 = 5, DAY6 = 6, DAY7 = 7} //List of game states
+    public enum TIME { THREEPM = 1, SIXPM = 2, NINEPM = 3, TWELVEPM = 4} //List of times
+
+    public DAY currentDay;
+    public TIME currentTime;
     void Start()
     {
-        
+        currentDay = DAY.DAY1;
+        currentTime = TIME.THREEPM;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressDay(int _index)
     {
-        
+        currentDay = (DAY)_index;
     }
+
+    void ProgressTime(int _index)
+    {
+        currentTime = (TIME)_index;
+    }
+    
 }
