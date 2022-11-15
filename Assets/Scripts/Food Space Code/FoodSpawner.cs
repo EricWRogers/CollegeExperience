5f0f5075be56c479878fada502491e72592c38d4
Aiden Thacker <aidenthacker86@gmail.com>
2022-11-15 16:47:00 -0600

    Added Sprites to Food Space Shooter

--- Assets/Scripts/Food Space Code/FoodSpawner.cs
+++ Assets/Scripts/Food Space Code/FoodSpawner.cs
@@ -6,8 +6,9 @@ public class FoodSpawner : MonoBehaviour
 {
 
     public List<GameObject> spawnPoint;
+    public List<Sprite> sprites;
 
-    public GameObject astorid;
+    public GameObject astroid;
     
     // Start is called before the first frame update
     void Start()
@@ -27,10 +28,11 @@ public class FoodSpawner : MonoBehaviour
     public void Spawn()
     {
         int index = Random.Range(0, 21);
-        GameObject a = Instantiate(astorid, spawnPoint[index].transform);
+        GameObject temp = Instantiate(astroid, spawnPoint[index].transform);
+        temp.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count - 1)];
 
         Vector2 lookDir = -(Vector2)spawnPoint[index].transform.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        a.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        temp.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
